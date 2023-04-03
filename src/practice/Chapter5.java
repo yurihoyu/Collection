ba88239ea254f0a4ca61426285aeca164b4f423d
YasukoDmm <112673301+yurihoyu@users.noreply.github.com>
2023-04-03 14:24:29 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -13,11 +13,11 @@ public class Chapter5 {
 		// ArrayListを生成する場合
 		List<Task> tasks = new ArrayList<Task>();
 
-		tasks.add(new Task(LocalDate.of(2021, 10, 21), ":牛乳を買う。"));
-		tasks.add(new Task(LocalDate.of(2021, 9, 15), ":○○社面談。"));
-		tasks.add(new Task(LocalDate.of(2021, 12, 4), ":手帳を買う。"));
-		tasks.add(new Task(LocalDate.of(2021, 8, 10), ":牛乳を買う。"));
-		tasks.add(new Task(LocalDate.of(2021, 11, 9), ":スクールの課題を解く。"));
+		tasks.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う。"));
+		tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談。"));
+		tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う。"));
+		tasks.add(new Task(LocalDate.of(2021, 8, 10), "牛乳を買う。"));
+		tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。"));
 
 
 
@@ -26,7 +26,7 @@ public class Chapter5 {
 
 
 		for (Task ts : tasks) {
-			  System.out.println(ts.getDate() + ts.getContents());
+			  System.out.println(ts.getDate() + ":" + ts.getContents());
 			}
 	}
 
